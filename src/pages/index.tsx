d2d3f28890cf587444009a735bda494847dec799
Jason Diaz Jimenez <developervulpo@outlook.com>
2023-01-01 17:56:21 -0600

    Fixed Import issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
 import About from "../components/Home/AboutMe/About";
 import Hero from "../components/Home/Hero/Hero";
-import Projects from "../components/Home/Projects/projects";
+import Projects from "../components/Home/Projects/Projects";
 import SkillsSection from "../components/Home/Skills/Skills";
 import Footer from "../components/Layout/Footer";
 import Layout from "../components/Layout/Layout";
-import Navbar from "../components/Layout/Navbar";
-import Project from "../components/Project/Project";
-import { projects as projectsList } from "../config/projects";
-import config from "../config/skills.json";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
