298eb42ed223509c948fa1eaa45cdabbcaa7876d
Jason Diaz Jimenez <vulpothedev@gmail.com>
2023-01-10 17:17:13 -0600

    Update SEO.tsx

--- app/src/components/SEO.tsx
+++ app/src/components/SEO.tsx
@@ -23,28 +23,28 @@ export default function SEO() {
         itemProp="image"
         content="https://mdbgo.io/ascensus/mdb-advanced/img/mdb.webp"
       />
-      <meta property="og:title" content="Bootstrap 5 & Material Design" />
+      <meta property="og:title" content="OzzyTheDev" />
       <meta
         property="og:description"
-        content="500+ material UI components. Free for personal and commercial use."
+        content="My Awesome Portfolio."
       />
       <meta
         property="og:image"
-        content="https://mdbgo.io/ascensus/mdb-advanced/img/mdb.webp"
+        content="https://v4.ozzy.gay/images/bg.png"
       />
-      <meta property="og:url" content="https://mdbootstrap.com/" />
-      <meta property="og:site_name" content="Material Design for Bootstrap" />
+      <meta property="og:url" content="https://ozzy.gay/" />
+      <meta property="og:site_name" content="OzzyTheDev Portfolio" />
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="website" />
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:title" content="Bootstrap 5 & Material Design" />
+      <meta property="twitter:title" content="OzzyTheDev" />
       <meta
         property="twitter:description"
-        content="Material Design for Bootstrap 5. 500+ components, free templates, 1-min installation, extensive tutorial, huge community"
+        content="My Portfolio"
       />
       <meta
         property="twitter:image:src"
-        content="https://mdbgo.io/ascensus/mdb-advanced/img/mdb.webp"
+        content="https://v4.ozzy.gay/images/bg.png"
       />
     </>
   );
