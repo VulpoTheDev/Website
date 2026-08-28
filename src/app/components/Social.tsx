28d7936239e991769ad810c64affe1ac303d25b4
Jason Diaz Jimenez <vulpothedev@gmail.com>
2026-08-28 18:30:57 -0500

    Add 'label' prop to Social component
    Added 'label' prop to Social component for better accessibility.

--- src/app/components/Social.tsx
+++ src/app/components/Social.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 
-export default function Social({ icon, link }: { icon: NonNullable<React.ReactElement>, link: string }) {
+export default function Social({ icon, link }: { icon: NonNullable<React.ReactElement>, link: string, label: string }) {
     const router = useRouter()
     return (
         <div onClick={() => router.push(link)}>
